f39375998ee4f5e82873901671e7ad486c5303e7
Erick Oduniyi <31193655+eoduniyi@users.noreply.github.com>
2023-03-31 11:38:37 -0400

    Add everything

--- collect_YT/src/App.jsx
+++ collect_YT/src/App.jsx
@@ -1,54 +1,27 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
-import ExpandCard from './components/ExpandCard'
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
+import ExpandCard from "./components/ExpandCard";
 
 function App() {
   return (
-<<<<<<< Updated upstream
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-=======
     <>
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact component={Home} />
+          <Route path="/" exact component={Home} />
           {/* <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} /> */}
         </Routes>
       </Router>
-      
     </>
   );
->>>>>>> Stashed changes
 }
 
 export default App;
